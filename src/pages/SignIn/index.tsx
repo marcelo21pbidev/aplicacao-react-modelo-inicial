5576f07b2f7423ce1dd939b9d55310df59f59348
Marcelo <marceloeduardo244@gmail.com>
2020-09-20 21:57:43 -0300

    commit 2

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,45 @@
 import React from "react";
 
-const SignIn: React.FC = () => <h1>SignIn</h1>;
+import { Container, Content, Background } from "./styles";
+
+import logoImg from "../../assets/logo.svg";
+
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="" />
+
+        <form>
+          <h1>Faça seu login</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="senha"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
+
+        <a href="cadastro">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
